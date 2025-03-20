1a7ee72f4f51039e0031d61ace547b42fb8c6dd0
Satyaa Goyal <satyaagoyal333333@gmail.com>
2025-03-20 23:59:26 +0530

    fixed  "azurerm_function_app_host_keys" "func" has not been declared in the root module.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,11 +8,11 @@ output "function_app_name" {
 
 # Use `azurerm_function_app_host_keys` for keys
 output "function_app_master_key" {
-  value = azurerm_function_app_host_keys.func.default_function_key
+  value = data.azurerm_function_app_host_keys.func.default_function_key
   sensitive = true
 }
 
 output "function_app_system_key" {
-  value = azurerm_function_app_host_keys.func.system_key
+  value = data.azurerm_function_app_host_keys.func.system_key
   sensitive = true
 }
