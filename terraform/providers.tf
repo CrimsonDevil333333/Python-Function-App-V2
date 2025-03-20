92b22bfee4964b4ceb160833bad6137994378cf8
Satyaa Goyal <satyaagoyal333333@gmail.com>
2025-03-20 22:55:01 +0530

    Removed redundent backends

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,13 +5,6 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "tf-backend-rg"
-    storage_account_name = "tfbackendstorage"
-    container_name       = "tfstate"
-    key                  = "azure-functions.tfstate"
-  }
 }
 
 provider "azurerm" {
